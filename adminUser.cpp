5509f3a76ee0bb04a42d020c50353d29349168e5
mchi7 <32309323+mchi7@users.noreply.github.com>
2018-11-07 21:25:52 -0500

    Add files via upload

---
+++ adminUser.cpp
@@ -0,0 +1,8 @@
+#include "adminUser.h"
+
+void adminUser::setType(std::string t) {
+	type = t;
+}
+String adminUser::getType() {
+	return type;
+}
---
+++ database.cpp
@@ -0,0 +1,28 @@
+#include "database.h"
+
+std::vector<person_class> list;
+
+database_class :: database_class(std::vector<person_class> a) {
+	list = a;
+}
+
+std::vector<person_class> database_class :: getList() {
+	return list;
+}
+
+person_class database_class :: getPerson(person_class person) {
+	int ID = person.getID();
+	for (uint i = 0; i < list.size(); i++) {
+		if (list[i].getID() == ID) {
+			return person;
+		}
+	}
+	cout << "Person was not found in the database" << endl;
+	return NULL;	
+}
+
+void database_class :: toString() {
+	for (uint i = 0; i < list.size(); i++) {
+		cout << "Person: " << list[i].getID() << "Age: " << list[i].getAge() << "Sex: " << list[i].getsex() << "Mood: " << list[i].getMood() << endl;
+	}
+}
