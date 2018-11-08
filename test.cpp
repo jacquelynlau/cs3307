bbe356e33d696e2c32af2d0bff92f04df459f134
Kyle Cheung <kcheu4@uwo.ca>
2018-11-08 18:44:21 -0500

    landmark done

--- test.cpp
+++ test.cpp
@@ -1,115 +1,132 @@
-// CPP program to detects face in a video 
 
 // Include required header files from OpenCV directory 
 #include "opencv2/objdetect.hpp" 
 #include "opencv2/highgui.hpp" 
 #include "opencv2/imgproc.hpp" 
 #include "opencv2/imgcodecs.hpp"
+#include "opencv2/face/facemark.hpp"
 #include <iostream> 
 
 using namespace std; 
 using namespace cv; 
 
 // Function for Face Detection 
-void detectAndDraw( Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedCascade, CascadeClassifier& nestedCascade2, double scale ); 
+void detectAndDraw( Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedCascade, double scale ); 
 string cascadeName, nestedCascadeName; 
 
 int main( int argc, const char** argv ) 
 { 
   // declares matrixs 
   // for now using static images, will try and upgrade to video capture later on.
-	Mat frame, image, song, dst; 
+
+  Mat frame, image, song, dst; 
   song = imread("song.jpg");
   resize(song, dst, Size(540,960));
-  
 
-	// PreDefined trained XML classifiers with facial features 
-	CascadeClassifier cascade, nestedCascade, nestedCascade2; 
-	double scale=1; 
+
+  // PreDefined trained XML classifiers with facial features 
+  CascadeClassifier cascade, nestedCascade;
+  double scale=1; 
 
   //load classifiers 
-  nestedCascade2.load("haarcascade_frontalface_default.xml");
-	nestedCascade.load( "haarcascade_eye_tree_eyeglasses.xml" ) ; 
-	cascade.load( "haarcascade_frontalcatface.xml" ) ; 
+  nestedCascade.load( "haarcascade_eye_tree_eyeglasses.xml" ) ; 
+  cascade.load( "haarcascade_frontalcatface.xml" ) ; 
 
-  detectAndDraw(dst, cascade, nestedCascade, nestedCascade2, scale);
+  detectAndDraw(dst, cascade, nestedCascade, scale);
 
-return 0; 
+  return 0; 
 } 
 
-void detectAndDraw( Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedCascade, CascadeClassifier& nestedCascade2, double scale) 
+void detectAndDraw( Mat& img, CascadeClassifier& cascade, CascadeClassifier& nestedCascade, double scale) 
 { 
-	vector<Rect> faces, faces2; 
-	Mat gray, smallImg; 
+  vector<Rect> faces, faces2; 
+  Mat gray, smallImg; 
+  Ptr<face::Facemark> facemark = face::createFacemarkKazemi();
+  perror("no error");
+  try{
 
-	cvtColor( img, gray, COLOR_BGR2GRAY ); // Convert to Gray Scale 
-	double fx = 1 / scale; 
+    facemark->loadModel("face_landmark_model.dat");
+  }
+  catch(const bad_alloc&){
+    perror("bad alloc here");
+  }
+  cout << "Loaded facemark model" << endl;
 
-	// Resize the Grayscale Image 
-	resize( gray, smallImg, Size(), fx, fx, INTER_LINEAR ); 
-	equalizeHist( smallImg, smallImg ); 
+  cvtColor( img, gray, COLOR_BGR2GRAY ); // Convert to Gray Scale 
+  double fx = 1 / scale; 
 
-	// Detect faces of different sizes using cascade classifier 
-	cascade.detectMultiScale( smallImg, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) ); 
+  // Resize the Grayscale Image 
+  resize( gray, smallImg, Size(), fx, fx, INTER_LINEAR_EXACT ); 
+  equalizeHist( smallImg, smallImg ); 
 
-	// Draw circles around the faces 
-	for ( size_t i = 0; i < faces.size(); i++ ) 
-	{ 
-		Rect r = faces[i]; 
-		Mat smallImgROI; 
-		vector<Rect> nestedObjects; 
-		Point center; 
-		Scalar color = Scalar(255, 0, 0); // Color for Drawing tool 
-		int radius; 
+  // Detect faces of different sizes using cascade classifier 
+  cascade.detectMultiScale( smallImg, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) ); 
 
-		double aspect_ratio = (double)r.width/r.height; 
-		if( 0.75 < aspect_ratio && aspect_ratio < 1.3 ) 
-		{ 
-			center.x = cvRound((r.x + r.width*0.5)*scale); 
-			center.y = cvRound((r.y + r.height*0.5)*scale); 
-			radius = cvRound((r.width + r.height)*0.25*scale); 
-			circle( img, center, radius, color, 3, 8, 0 ); 
-		} 
-		else
-			rectangle( img, cvPoint(cvRound(r.x*scale), cvRound(r.y*scale)), 
-					cvPoint(cvRound((r.x + r.width-1)*scale), 
-					cvRound((r.y + r.height-1)*scale)), color, 3, 8, 0); 
-		if( nestedCascade.empty() ) 
-			continue; 
-		smallImgROI = smallImg( r ); 
-		
-		// Detection of eyes int the input image 
-		nestedCascade.detectMultiScale( smallImgROI, nestedObjects, 1.1, 2, 
-										0|CASCADE_SCALE_IMAGE, Size(30, 30) ); 
-		
-		// Draw circles around eyes 
-		for ( size_t j = 0; j < nestedObjects.size(); j++ ) 
-		{ 
-			Rect nr = nestedObjects[j]; 
-			center.x = cvRound((r.x + nr.x + nr.width*0.5)*scale); 
-			center.y = cvRound((r.y + nr.y + nr.height*0.5)*scale); 
-			radius = cvRound((nr.width + nr.height)*0.25*scale); 
-			circle( img, center, radius, color, 3, 8, 0 ); 
-			
-		// Detection of face
-		nestedCascade2.detectMultiScale( smallImgROI, nestedObjects, 1.1, 2, 
-										0|CASCADE_SCALE_IMAGE, Size(30, 30) ); 
-		
-		for ( size_t k = 0; k < nestedObjects.size(); k++ ) 
-		{ 
-			Rect nr = nestedObjects[k]; 
-			center.x = cvRound((r.x + nr.x + nr.width*0.5)*scale); 
-			center.y = cvRound((r.y + nr.y + nr.height*0.5)*scale); 
-			radius = cvRound((nr.width + nr.height)*0.25*scale); 
-			circle( img, center, radius, color, 3, 8, 0 ); 
-		} 
-} 
-	} 
+  // Draw circles around the faces 
+  for ( size_t i = 0; i < faces.size(); i++ ) 
+  { 
+    Rect r = faces[i]; 
+    Mat smallImgROI; 
+    vector<Rect> nestedObjects; 
+    Point center; 
+    Scalar color = Scalar(255, 0, 0); // Color for Drawing tool 
+    int radius; 
 
-	// Show Processed Image with detected faces 
+    double aspect_ratio = (double)r.width/r.height; 
+    if( 0.75 < aspect_ratio && aspect_ratio < 1.3 ) 
+    { 
+      center.x = cvRound((r.x + r.width*0.5)*scale); 
+      center.y = cvRound((r.y + r.height*0.5)*scale); 
+      radius = cvRound((r.width + r.height)*0.25*scale); 
+      circle( img, center, radius, color, 3, 8, 0 ); 
+    } 
+    else
+      rectangle( img, cvPoint(cvRound(r.x*scale), cvRound(r.y*scale)), 
+          cvPoint(cvRound((r.x + r.width-1)*scale), 
+            cvRound((r.y + r.height-1)*scale)), color, 3, 8, 0); 
+    if( nestedCascade.empty() ) 
+      continue; 
+    smallImgROI = smallImg( r ); 
+
+    // Detection of eyes int the input image 
+    nestedCascade.detectMultiScale( smallImgROI, nestedObjects, 1.1, 2, 
+        0|CASCADE_SCALE_IMAGE, Size(30, 30) ); 
+
+    // Draw circles around eyes 
+    for ( size_t j = 0; j < nestedObjects.size(); j++ ) 
+    { 
+      Rect nr = nestedObjects[j]; 
+      center.x = cvRound((r.x + nr.x + nr.width*0.5)*scale); 
+      center.y = cvRound((r.y + nr.y + nr.height*0.5)*scale); 
+      radius = cvRound((nr.width + nr.height)*0.25*scale); 
+      circle( img, center, radius, color, 3, 8, 0 ); 
+
+
+
+    } 
+  } 
+
+  vector< vector<Point2f> > shapes;
+
+  if (facemark->fit(img, faces, shapes))
+  {
+    for (size_t n = 0; n < faces.size(); n++)
+    {
+      rectangle(img,faces[n], Scalar(255,0,0));
+    }
+    for(unsigned long n=0; n <faces.size(); n++){
+      for(unsigned long k=0; k < shapes[n].size(); k++)
+      {
+        circle(img, shapes[n][k], 5, Scalar(0,0,255), FILLED);
+      }
+    }
+
+  }
+
+
   namedWindow ("Display window", WINDOW_AUTOSIZE);
-	imshow( "Display window", img ); 
-  // wait for key before exit
+  imshow( "Display window", img ); 
+  //wait for key before exit
   waitKey(0);
 } 
 
